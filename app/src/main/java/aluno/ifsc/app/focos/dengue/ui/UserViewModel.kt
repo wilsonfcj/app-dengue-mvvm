9fc4f9670eece464a3bbfceebc4996ab17546c51
Milton <milton.dev.sis@gmail.com>
2022-03-28 10:49:08 -0300

    Commit test

--- app/src/main/java/aluno/ifsc/app/focos/dengue/ui/UserViewModel.kt
+++ app/src/main/java/aluno/ifsc/app/focos/dengue/ui/UserViewModel.kt
@@ -5,12 +5,9 @@ import aluno.ifsc.app.focos.dengue.data.resources.request.UserRequest
 import aluno.ifsc.app.focos.dengue.data.resources.response.BaseResponse
 import aluno.ifsc.app.focos.dengue.data.resources.response.UserResponse
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
 import javax.inject.Inject
-
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
@@ -19,9 +16,8 @@ class UserViewModel @Inject constructor(
 
     private var loginResult: Flow<BaseResponse<UserResponse.Login>>? = null
 
-    fun login(query : UserRequest.Login) : Flow<BaseResponse<UserResponse.Login>> {
-        val newResult: Flow<BaseResponse<UserResponse.Login>> = repository.authLoginUser(query)
-        return newResult
+    fun login(query: UserRequest.Login): Flow<BaseResponse<UserResponse.Login>> {
+        return repository.authLoginUser(query)
     }
 
 }
